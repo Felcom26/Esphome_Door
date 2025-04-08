5c33b25c0f1df6c0c1a91ea12b3187f1174874a7
Felipe Freitas <felcomfts@gmail.com>
2025-04-08 12:18:38 -0300

    print config

--- esphome/components/auto_door/display.py
+++ esphome/components/auto_door/display.py
@@ -28,8 +28,8 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
         cv.Required(CONF_POT_PIN): gpio_pin_schema,
         cv.Required(CONF_ES_OFF_PIN): gpio_pin_schema,
         cv.Required(CONF_ES_ON_PIN): gpio_pin_schema,
-        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_range(min=0, max=270),
-        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_range(min=0, max=270),
+        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_,
+        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_,
     }
 ).extend(cv.polling_component_schema("50ms"))
 
@@ -59,11 +59,24 @@ async def to_code(config):
     eson_pin = await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])
     cg.add(var.set_eson_pin(eson_pin))
 
-    cg.add(var.set_ang_open(config[CONF_ANG_OPEN]))
-    cg.add(var.set_ang_close(config[CONF_ANG_CLOSE]))
+    open_angle = config[CONF_ANG_OPEN]
+    if open_angle < 0 or open_angle > 270:
+        raise cv.Invalid(f"ang_open out of range (0-270): {open_angle}")
+    cg.add(var.set_ang_open(open_angle))
+
+    close_angle = config[CONF_ANG_CLOSE]
+    if close_angle < 0 or close_angle > 270:
+        raise cv.Invalid(f"ang_close out of range (0-270): {close_angle}")
+    cg.add(var.set_ang_close(close_angle))
+
+    # cg.add(var.set_ang_open(config[CONF_ANG_OPEN]))
+    # cg.add(var.set_ang_close(config[CONF_ANG_CLOSE]))
 
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
             config[CONF_LAMBDA], [(AUTODOORComponentRef, "it")], return_type=cg.void
         )
         cg.add(var.set_writer(lambda_))
+
+    print("DEBUG CONFIG:")
+    print(config)
