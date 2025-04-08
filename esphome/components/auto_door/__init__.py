ce0472be758cc5d5356ef0e32227c45b1c6a2124
Felipe Freitas <felcomfts@gmail.com>
2025-04-08 17:30:15 -0300

    com posiçãoaaaaa

--- esphome/components/auto_door/__init__.py
+++ esphome/components/auto_door/__init__.py
@@ -1,5 +1,7 @@
 # from esphome import automation
 import esphome.codegen as cg
+
+# from esphome.components import sensor
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
 
