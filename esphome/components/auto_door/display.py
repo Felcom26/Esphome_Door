cd7af2c1c547ae309e030383af2634f23e24191b
Felipe Freitas <felcomfts@gmail.com>
2025-04-08 12:36:22 -0300

    gpt mudou mais coisa

--- esphome/components/auto_door/display.py
+++ esphome/components/auto_door/display.py
@@ -28,8 +28,9 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
         cv.Required(CONF_POT_PIN): gpio_pin_schema,
         cv.Required(CONF_ES_OFF_PIN): gpio_pin_schema,
         cv.Required(CONF_ES_ON_PIN): gpio_pin_schema,
-        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_,
-        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_,
+        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_range(min=0, max=270),
+        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_range(min=0, max=270),
+        cv.Optional(CONF_LAMBDA): cv.lambda_,
     }
 ).extend(cv.polling_component_schema("50ms"))
 
@@ -38,45 +39,44 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await display.register_display(var, config)
 
-    drive_pin = await cg.gpio_pin_expression(config[CONF_DRIVE_PIN])
-    cg.add(var.set_drive_pin(drive_pin))
+    # drive_pin = await cg.gpio_pin_expression(config[CONF_DRIVE_PIN])
+    # cg.add(var.set_drive_pin(drive_pin))
 
-    dir_pin = await cg.gpio_pin_expression(config[CONF_DIR_PIN])
-    cg.add(var.set_dir_pin(dir_pin))
+    # dir_pin = await cg.gpio_pin_expression(config[CONF_DIR_PIN])
+    # cg.add(var.set_dir_pin(dir_pin))
 
-    rotsen_pin = await cg.gpio_pin_expression(config[CONF_ROT_SENSOR_PIN])
-    cg.add(var.set_rotsen_pin(rotsen_pin))
+    # rotsen_pin = await cg.gpio_pin_expression(config[CONF_ROT_SENSOR_PIN])
+    # cg.add(var.set_rotsen_pin(rotsen_pin))
 
-    engage_pin = await cg.gpio_pin_expression(config[CONF_ENGAGE_MOTOR_PIN])
-    cg.add(var.set_engage_pin(engage_pin))
+    # engage_pin = await cg.gpio_pin_expression(config[CONF_ENGAGE_MOTOR_PIN])
+    # cg.add(var.set_engage_pin(engage_pin))
 
-    pot_pin = await cg.gpio_pin_expression(config[CONF_POT_PIN])
-    cg.add(var.set_pot_pin(pot_pin))
+    # pot_pin = await cg.gpio_pin_expression(config[CONF_POT_PIN])
+    # cg.add(var.set_pot_pin(pot_pin))
 
-    esoff_pin = await cg.gpio_pin_expression(config[CONF_ES_OFF_PIN])
-    cg.add(var.set_esoff_pin(esoff_pin))
+    # esoff_pin = await cg.gpio_pin_expression(config[CONF_ES_OFF_PIN])
+    # cg.add(var.set_esoff_pin(esoff_pin))
 
-    eson_pin = await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])
-    cg.add(var.set_eson_pin(eson_pin))
+    # eson_pin = await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])
+    # cg.add(var.set_eson_pin(eson_pin))
 
-    open_angle = config[CONF_ANG_OPEN]
-    if open_angle < 0 or open_angle > 270:
-        raise cv.Invalid(f"ang_open out of range (0-270): {open_angle}")
-    cg.add(var.set_ang_open(open_angle))
+    cg.add(var.set_drive_pin(await cg.gpio_pin_expression(config[CONF_DRIVE_PIN])))
+    cg.add(var.set_dir_pin(await cg.gpio_pin_expression(config[CONF_DIR_PIN])))
+    cg.add(
+        var.set_rotsen_pin(await cg.gpio_pin_expression(config[CONF_ROT_SENSOR_PIN]))
+    )
+    cg.add(
+        var.set_engage_pin(await cg.gpio_pin_expression(config[CONF_ENGAGE_MOTOR_PIN]))
+    )
+    cg.add(var.set_pot_pin(await cg.gpio_pin_expression(config[CONF_POT_PIN])))
+    cg.add(var.set_esoff_pin(await cg.gpio_pin_expression(config[CONF_ES_OFF_PIN])))
+    cg.add(var.set_eson_pin(await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])))
 
-    close_angle = config[CONF_ANG_CLOSE]
-    if close_angle < 0 or close_angle > 270:
-        raise cv.Invalid(f"ang_close out of range (0-270): {close_angle}")
-    cg.add(var.set_ang_close(close_angle))
-
-    # cg.add(var.set_ang_open(config[CONF_ANG_OPEN]))
-    # cg.add(var.set_ang_close(config[CONF_ANG_CLOSE]))
+    cg.add(var.set_ang_open(config[CONF_ANG_OPEN]))
+    cg.add(var.set_ang_close(config[CONF_ANG_CLOSE]))
 
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
             config[CONF_LAMBDA], [(AUTODOORComponentRef, "it")], return_type=cg.void
         )
         cg.add(var.set_writer(lambda_))
-
-    print("DEBUG CONFIG:")
-    print(config)
