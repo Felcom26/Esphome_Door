3427823d7505cdc4549f33a4904c049925765382
Felipe Freitas <felcomfts@gmail.com>
2025-04-08 13:00:01 -0300

    pinos internos

--- esphome/components/auto_door/display.py
+++ esphome/components/auto_door/display.py
@@ -7,29 +7,28 @@ auto_door_ns = cg.esphome_ns.namespace("auto_door")
 AUTODOORComponent = auto_door_ns.class_("AUTODOORComponent", cg.PollingComponent)
 AUTODOORComponentRef = AUTODOORComponent.operator("ref")
 
-CONF_DRIVE_PIN = "drive_pin"
-CONF_DIR_PIN = "dir_pin"
-CONF_ROT_SENSOR_PIN = "rotsen_pin"
-CONF_ENGAGE_MOTOR_PIN = "engage_pin"
-CONF_POT_PIN = "pot_pin"
-CONF_ES_OFF_PIN = "esoff_pin"
-CONF_ES_ON_PIN = "eson_pin"
+# CONF_DRIVE_PIN = "drive_pin"
+# CONF_DIR_PIN = "dir_pin"
+# CONF_ROT_SENSOR_PIN = "rotsen_pin"
+# CONF_ENGAGE_MOTOR_PIN = "engage_pin"
+# CONF_POT_PIN = "pot_pin"
+# CONF_ES_OFF_PIN = "esoff_pin"
+# CONF_ES_ON_PIN = "eson_pin"
 CONF_ANG_OPEN = "ang_open"
 CONF_ANG_CLOSE = "ang_close"
 
 CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend(
     {
         cv.GenerateID(): cv.declare_id(AUTODOORComponent),
-        cv.Required(CONF_DRIVE_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_DIR_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_ROT_SENSOR_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_ENGAGE_MOTOR_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_POT_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_ES_OFF_PIN): cv.templatable(cv.string),
-        cv.Required(CONF_ES_ON_PIN): cv.templatable(cv.string),
-        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_range(min=0, max=270),
-        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_range(min=0, max=270),
-        cv.Optional(CONF_LAMBDA): cv.lambda_,
+        # cv.Required(CONF_DRIVE_PIN): gpio_pin_schema,
+        # cv.Required(CONF_DIR_PIN): gpio_pin_schema,
+        # cv.Required(CONF_ROT_SENSOR_PIN): gpio_pin_schema,
+        # cv.Required(CONF_ENGAGE_MOTOR_PIN): gpio_pin_schema,
+        # cv.Required(CONF_POT_PIN): gpio_pin_schema,
+        # cv.Required(CONF_ES_OFF_PIN): gpio_pin_schema,
+        # cv.Required(CONF_ES_ON_PIN): gpio_pin_schema,
+        cv.Optional(CONF_ANG_OPEN, default=238): cv.int_,
+        cv.Optional(CONF_ANG_CLOSE, default=118): cv.int_,
     }
 ).extend(cv.polling_component_schema("50ms"))
 
@@ -58,18 +57,6 @@ async def to_code(config):
 
     # eson_pin = await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])
     # cg.add(var.set_eson_pin(eson_pin))
-
-    cg.add(var.set_drive_pin(await cg.gpio_pin_expression(config[CONF_DRIVE_PIN])))
-    cg.add(var.set_dir_pin(await cg.gpio_pin_expression(config[CONF_DIR_PIN])))
-    cg.add(
-        var.set_rotsen_pin(await cg.gpio_pin_expression(config[CONF_ROT_SENSOR_PIN]))
-    )
-    cg.add(
-        var.set_engage_pin(await cg.gpio_pin_expression(config[CONF_ENGAGE_MOTOR_PIN]))
-    )
-    cg.add(var.set_pot_pin(await cg.gpio_pin_expression(config[CONF_POT_PIN])))
-    cg.add(var.set_esoff_pin(await cg.gpio_pin_expression(config[CONF_ES_OFF_PIN])))
-    cg.add(var.set_eson_pin(await cg.gpio_pin_expression(config[CONF_ES_ON_PIN])))
 
     cg.add(var.set_ang_open(config[CONF_ANG_OPEN]))
     cg.add(var.set_ang_close(config[CONF_ANG_CLOSE]))
