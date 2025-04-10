94d8b518da523e7b04a71b5c0f159acb234125b7
Felipe Freitas <felcomfts@gmail.com>
2025-04-10 08:31:16 -0300

    debug_loop

--- esphome/components/auto_door/auto_door.cpp
+++ esphome/components/auto_door/auto_door.cpp
@@ -111,12 +111,15 @@ void AUTODOORComponent::dump_config() {
 }
 
 void AUTODOORComponent::loop() {
+  uint32_t start = millis();
   pot = analogRead(pot_pin_);
   ES_off = digitalRead(esoff_pin_);
   ES_on = digitalRead(eson_pin_);
 
   pos = map(pot, 4096, 0, 0, 270);
   ha_pos = map(pos, ang_close_, ang_open_, 0, 100);
+
+  ESP_LOGD("auto_door", "Loop time: %u ms", millis() - start);
 
   if (cmd == 'e' && Estado_EM == false) {
     f_e = 1;
